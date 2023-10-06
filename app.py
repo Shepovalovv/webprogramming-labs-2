d7c31c8570e9afba20d6cd2d53f8f76d6c9e6359
Шеповалов Артём <Shepovalov03@bk.ru>
2023-10-06 22:23:16 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на Flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <tittle>Шеповалов Артём Сергеевич, Лабораторная 1<tittle>
+    </head>
+    <body>
+        <header>
+        НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервис на flask</h1>
+
+        <footer>
+        &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
