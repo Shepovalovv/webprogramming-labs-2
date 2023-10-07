df89056e7db01d0743d8fb36b67a72d73c4b2ad2
Шеповалов Артём <Shepovalov03@bk.ru>
2023-10-07 13:22:10 +0700

    Роуты

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ def start():
 
 @app.route("/menu")
 def menu():
-    return """
+    return '''
 <!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
 <html>
     <head>
         <title>НГТУ, ФБ, Лабораторные работы</title>
@@ -19,23 +20,24 @@ def menu():
         НГТУ,ФБ, WEB-программирование, часть 2. Список лабораторных
         </header>
 
-        <ol>
+        <ul>
             <li>
             <a href = '/lab1' target = _blank>Первая Лабораторная работа</a>
             </li>
-        </ol>
+        </ul>
 
         <footer>
         &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
         </footer>
     </body>
 </html>
-"""
+'''
 
 @app.route("/lab1")
 def lab1():
-    return """
+    return '''
 <!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
 <html>
     <head>
         <title>Шеповалов Артём Сергеевич, Лабораторная 1</title>
@@ -46,6 +48,12 @@ def lab1():
         </header>
 
         <h1>web-сервис на flask</h1>
+        <h2>Реализованные роуты</h2>
+
+        <a href = "/lab1/oak" target =_blank>Дуб</a><br>
+        <a href = "/lab1/student" target =_blank>Студент</a><br>
+        <a href = "/lab1/python" target =_blank>Python</a><br>
+        <a href = "/lab1/LoL" target =_blank>LoL</a><br>
 
         <div>
         Flask — фреймворк для создания веб-приложений на языке
@@ -55,12 +63,14 @@ def lab1():
         веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
         </div>
 
+        <a href ="/menu" target =_blank>Меню</a>
+
         <footer>
         &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
         </footer>
     </body>
 </html>
-"""
+'''
 
 @app.route("/lab1/oak")
 def oak():
@@ -74,3 +84,52 @@ def oak():
     </body>
 <html>
 '''
+
+@app.route("/lab1/student")
+def student():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <h1>Шеповалов Артём Сергеевич</h1>
+        <img src ="''' + url_for('static', filename = 'logo.jpg') +'''">
+    </body>
+<html>
+'''
+
+@app.route("/lab1/python")
+def python():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <div>
+            Автор языка Python назвал его в честь британского комедийного шоу «Monty Python»,
+            которое было популярно в начале 1970-х годов.<br>
+            Это телешоу позволяло автору расслабиться и отвлечься от разработки языка.<br>
+            Однако, несмотря на настоящее происхождение названия, для людей более очевидно
+            связывать Python со словом «змея».<br> Этому также способствует логотип, на котором изображена рептилия.
+        </div>
+        <img src ="''' + url_for('static', filename = 'python.png') +'''">
+    </body>
+<html>
+'''
+
+@app.route("/lab1/LoL")
+def LoL():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <div>
+            Когда играешь в эту игру, теряешь свои нервы, изматываешь душу.<br>
+            Однако, она затягивает, как сделка с дьяволом...<br>
+        </div>
+        <img src ="''' + url_for('static', filename = 'LoL.jpg') +'''">
+    </body>
+<html>
+'''
+
