0245a681955632d4751ea1f4a4da84881990acf1
Шеповалов Артём <Shepovalov03@bk.ru>
2023-10-09 10:45:51 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -132,4 +132,6 @@ def LoL():
     </body>
 <html>
 '''
-
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
