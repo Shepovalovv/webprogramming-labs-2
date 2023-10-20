ee612dec74313881097fb52c863744e28d78501d
Шеповалов Артём <Shepovalov03@bk.ru>
2023-10-20 23:42:25 +0700

    Рефакторинг: выделение первой лабы + (убрал венв из гитигнор)

--- app.py
+++ app.py
@@ -1,152 +1,20 @@
 from flask import Flask, redirect, url_for, render_template
+from lab1 import lab1
+
 app = Flask(__name__)
+app.register_blueprint(lab1)
 
-@app.route("/")
-@app.route("/index")
-def start():
-    return redirect("/menu", code=302)
-
-@app.route("/menu")
-def menu():
-    return '''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <head>
-        <title>НГТУ, ФБ, Лабораторные работы</title>
-    </head>
-    <body>
-        <header>
-        НГТУ,ФБ, WEB-программирование, часть 2. Список лабораторных
-        </header>
-
-        <ul>
-            <li>
-                <a href = '/lab1' target = _blank>Первая Лабораторная работа</a>
-            </li>
-
-            <li>
-                <a href = '/lab2' target = _blank>Вторая Лабораторная работа</a>
-            </li>
-        </ul>
-
-        <footer>
-        &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-'''
-
-@app.route("/lab1")
-def lab1():
-    return '''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <head>
-        <title>Шеповалов Артём Сергеевич, Лабораторная 1</title>
-    </head>
-    <body>
-        <header>
-        НГТУ, ФБ, Лабораторная работа 1
-        </header>
-
-        <h1>web-сервис на flask</h1>
-        <h2>Реализованные роуты</h2>
-
-        <a href = "/lab1/oak" target =_blank>Дуб</a><br>
-        <a href = "/lab1/student" target =_blank>Студент</a><br>
-        <a href = "/lab1/python" target =_blank>Python</a><br>
-        <a href = "/lab1/LoL" target =_blank>LoL</a><br>
-
-        <div>
-        Flask — фреймворк для создания веб-приложений на языке
-        программирования Python, использующий набор инструментов
-        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
-        называемых микрофреймворков — минималистичных каркасов
-        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
-        </div>
-
-        <a href ="/menu" target =_blank>Меню</a>
-
-        <footer>
-        &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
-        </footer>
-    </body>
-</html>
-'''
-
-@app.route("/lab1/oak")
-def oak():
-    return'''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <body>
-        <h1>Дуб</h1>
-        <img src ="''' + url_for('static', filename = 'oak.jpg') +'''">
-    </body>
-<html>
-'''
-
-@app.route("/lab1/student")
-def student():
-    return'''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <body>
-        <h1>Шеповалов Артём Сергеевич</h1>
-        <img src ="''' + url_for('static', filename = 'logo.jpg') +'''">
-    </body>
-<html>
-'''
-
-@app.route("/lab1/python")
-def python():
-    return'''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <body>
-        <div>
-            Автор языка Python назвал его в честь британского комедийного шоу «Monty Python»,
-            которое было популярно в начале 1970-х годов.<br>
-            Это телешоу позволяло автору расслабиться и отвлечься от разработки языка.<br>
-            Однако, несмотря на настоящее происхождение названия, для людей более очевидно
-            связывать Python со словом «змея».<br> Этому также способствует логотип, на котором изображена рептилия.
-        </div>
-        <img src ="''' + url_for('static', filename = 'python.png') +'''">
-    </body>
-<html>
-'''
-
-@app.route("/lab1/LoL")
-def LoL():
-    return'''
-<!doctype html>
-<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
-<html>
-    <body>
-        <div>
-            Когда играешь в эту игру, теряешь свои нервы, изматываешь душу.<br>
-            Однако, она затягивает, как сделка с дьяволом...<br>
-        </div>
-        <img src ="''' + url_for('static', filename = 'LoL.jpg') +'''">
-    </body>
-<html>
-'''
 
 @app.route('/lab2/example')
 def example():
     name, labnumber, group, cource = "Шеповалов Артём Сергеевич", 2, "ФБИ-11", "3 курс"
     fruits = [
     {'name': 'яблоки', 'price': 100},
-     {'name': 'груши', 'price': 120}, 
-     {'name': 'апельсины', 'price': 80}, 
-     {'name': 'мандарины', 'price': 95}, 
-     {'name': 'манго', 'price': 95}
-     ]
+    {'name': 'груши', 'price': 120}, 
+    {'name': 'апельсины', 'price': 80}, 
+    {'name': 'мандарины', 'price': 95}, 
+    {'name': 'манго', 'price': 95}
+    ]
     
     books = [
     {'autors': 'Игорь Валериев', 'book': 'Ермак. Личник', 'cat': 'Историческая фантастика', 'str': '333 страниц'},
@@ -166,7 +34,7 @@ def example():
                             cource = cource, fruits = fruits, books = books)
 
 @app.route('/lab2/')
-def lab2():
+def lab():
     return render_template('lab2.html')
 
 @app.route('/lab2/lol')
---
+++ lab1.py
@@ -0,0 +1,145 @@
+from flask import Blueprint, redirect, url_for
+lab1 = Blueprint('lab1', __name__)
+
+
+@lab1.route("/")
+@lab1.route("/index")
+def start():
+    return redirect("/menu", code=302)
+
+
+@lab1.route("/menu")
+def menu():
+    return '''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <head>
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+        НГТУ,ФБ, WEB-программирование, часть 2. Список лабораторных
+        </header>
+
+        <ul>
+            <li>
+                <a href = '/lab1' target = _blank>Первая Лабораторная работа</a>
+            </li>
+
+            <li>
+                <a href = '/lab2' target = _blank>Вторая Лабораторная работа</a>
+            </li>
+        </ul>
+
+        <footer>
+        &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+'''
+
+
+@lab1.route("/lab1")
+def lab():
+    return '''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <head>
+        <title>Шеповалов Артём Сергеевич, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+        НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервис на flask</h1>
+        <h2>Реализованные роуты</h2>
+
+        <a href = "/lab1/oak" target =_blank>Дуб</a><br>
+        <a href = "/lab1/student" target =_blank>Студент</a><br>
+        <a href = "/lab1/python" target =_blank>Python</a><br>
+        <a href = "/lab1/LoL" target =_blank>LoL</a><br>
+
+        <div>
+        Flask — фреймворк для создания веб-приложений на языке
+        программирования Python, использующий набор инструментов
+        Werkzeug, а также шаблонизатор Jinja2. Относится к категории так
+        называемых микрофреймворков — минималистичных каркасов
+        веб-приложений, сознательно предоставляющих лишь самые базовые возможности.
+        </div>
+
+        <a href ="/menu" target =_blank>Меню</a>
+
+        <footer>
+        &copy; Шеповалов Артём, ФБИ-11, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+'''
+
+
+@lab1.route("/lab1/oak")
+def oak():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <h1>Дуб</h1>
+        <img src ="''' + url_for('static', filename = 'oak.jpg') +'''">
+    </body>
+<html>
+'''
+
+
+@lab1.route("/lab1/student")
+def student():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <h1>Шеповалов Артём Сергеевич</h1>
+        <img src ="''' + url_for('static', filename = 'logo.jpg') +'''">
+    </body>
+<html>
+'''
+
+
+@lab1.route("/lab1/python")
+def python():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <div>
+            Автор языка Python назвал его в честь британского комедийного шоу «Monty Python»,
+            которое было популярно в начале 1970-х годов.<br>
+            Это телешоу позволяло автору расслабиться и отвлечься от разработки языка.<br>
+            Однако, несмотря на настоящее происхождение названия, для людей более очевидно
+            связывать Python со словом «змея».<br> Этому также способствует логотип, на котором изображена рептилия.
+        </div>
+        <img src ="''' + url_for('static', filename = 'python.png') +'''">
+    </body>
+<html>
+'''
+
+
+@lab1.route("/lab1/LoL")
+def LoL():
+    return'''
+<!doctype html>
+<link rel="stylesheet" href="''' + url_for('static', filename='lab1.css') + '''">
+<html>
+    <body>
+        <div>
+            Когда играешь в эту игру, теряешь свои нервы, изматываешь душу.<br>
+            Однако, она затягивает, как сделка с дьяволом...<br>
+        </div>
+        <img src ="''' + url_for('static', filename = 'LoL.jpg') +'''">
+    </body>
+<html>
+'''
