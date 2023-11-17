3d2d2207f961c70b9e1d67e028cca23aa3bf6591
Шеповалов Артём <Shepovalov03@bk.ru>
2023-11-18 00:51:53 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -1,7 +1,31 @@
-from flask import Blueprint, redirect, url_for, render_template
+from flask import Blueprint, redirect, url_for, render_template, request
 lab4 = Blueprint('lab4', __name__)
 
 
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4.html')
+
+
+@lab4.route('/lab4/login', methods = ['GET', 'POST'])
+def login():
+    error = {}
+    if request.method == 'GET':
+        return render_template("login.html", error = error)
+    
+    
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == 'alex' and password == '123':
+        return render_template('success2.html')
+    
+
+    error['userpass'] = 'Неверные логин и/или пароль'
+    if  username == '':
+        error['emptyuser'] = 'не введён логин'
+
+    if password == '':
+        error['emptypass'] = 'Не введён пароль'
+    return render_template('login.html', error=error, username=username, password=password)
+
+    
