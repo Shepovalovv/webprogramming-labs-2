33819c9a8a3fa8be4ee3b8ff3c1622d1c0fcce8b
Шеповалов Артём <Shepovalov03@bk.ru>
2023-12-01 18:49:19 +0700

    Холодильник

--- lab4.py
+++ lab4.py
@@ -29,3 +29,14 @@ def login():
     return render_template('login.html', error=error, username=username, password=password)
 
     
+@lab4.route('/lab4/fridge', methods=['GET', 'POST'])
+def fridge():
+    errors = {}
+    if request.method == 'GET':
+        return render_template("fridge.html", errors=errors)
+    temp = request.form.get('temp')
+    if temp:
+        temp = int(temp)
+    return render_template("fridge.html", temp=temp, errors=errors)
+
+
